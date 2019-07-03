920dcb2e045c425ab1053e5a2bc728e8648cea6b
carlosgarcia <carloja@gmail.com>
2019-07-03 08:12:58 +0200

    versión inicial

---
+++ app.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+//import ComponenteFuncional1 from './components/ComponenteFuncional1'
+
+const App = () => (
+    <div>
+        <h1>Hola mundo </h1>
+    </div>
+);
+
+ReactDOM.render(<App />,document.getElementById('root'));
---
+++ components/ComponenteFuncional1.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const ComponenteFuncional1 = () => {
+    <div>
+        Componente Funcional 1
+    </div>
+}
+
+export default ComponenteFuncional1
